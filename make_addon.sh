647848302aca6b3f9c8c201089ef9eaa55020221
douira <douira@users.noreply.github.com>
2018-05-23 17:23:26 +0200

    added some status messages

--- make_addon.sh
+++ make_addon.sh
@@ -2,11 +2,15 @@
 #This file was created by https://github.com/douira
 #requires cd, git, cmake, zip, mv to be installed
 #Works with https://github.com/rlguy/Blender-FLIP-Fluids
+echo "Downloading..."
 cd "${0%/*}"
 git clone https://github.com/rlguy/Blender-FLIP-Fluids.git
+echo "Making..."
 cd Blender-FLIP-Fluids
 cmake CMakeLists.txt
 make
+echo "Preparing as add-on..."
 cd bl_flip_fluids/
 zip -r flip_fluids_addon.zip flip_fluids_addon
 mv flip_fluids_addon.zip ../../flip_fluids_addon.zip
+echo "Done."
