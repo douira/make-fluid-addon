68d5e677f5f29b2b4359d3a686225fe27c2b8c9e
douira <douira@users.noreply.github.com>
2018-05-23 17:13:14 +0200

    added script file

---
+++ make_addon.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#This file was created by https://github.com/douira
+#requires cmake, make, cd, zip, mv to be installed
+#Works with https://github.com/rlguy/Blender-FLIP-Fluids
+cd "${0%/*}"
+git clone https://github.com/rlguy/Blender-FLIP-Fluids.git
+cd Blender-FLIP-Fluids
+cmake CMakeLists.txt
+make
+cd bl_flip_fluids/
+zip -r flip_fluids_addon.zip flip_fluids_addon
+mv flip_fluids_addon.zip ../../flip_fluids_addon.zip
